8c59361d6d9874b709dd877c9e16f79bfd482787
Han Tuzun <hantuzun@users.noreply.github.com>
2023-02-09 01:19:18 +0300

    Add example data for organizations

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,91 @@
 export default function Home() {
   const metrics = [
-    { id: 1, stat: '7.8 and 7.5 Mww ', emphasis: 'Magnitudes', rest: 'of the mainshock and the largest aftershock.' , source: 'https://en.wikipedia.org/wiki/2023_Turkey%E2%80%93Syria_earthquake'},
-    { id: 2, stat: '11,700+', emphasis: 'Death toll', rest: 'from the latest official figures.' , source: 'https://www.nytimes.com/live/2023/02/08/world/turkey-syria-earthquake'},
-    { id: 3, stat: '3,450+', emphasis: 'Collapsed buildings', rest: 'on people trapped in the ruble.' , source: 'https://www.nytimes.com/2023/02/05/world/europe/turkey-earthquake-rescue.html'},
-    { id: 4, stat: '23 million', emphasis: 'people affected', rest: 'from the earthquakes.' , source: 'https://abcnews.go.com/International/live-updates/turkey-earthquake/?id=96913081#96947062'},
+    { stat: '7.8 and 7.5 Mww ', emphasis: 'Magnitudes', rest: 'of the mainshock and the largest aftershock.' , source: 'https://en.wikipedia.org/wiki/2023_Turkey%E2%80%93Syria_earthquake'},
+    { stat: '11,700+', emphasis: 'Death toll', rest: 'from the latest official figures.' , source: 'https://www.nytimes.com/live/2023/02/08/world/turkey-syria-earthquake'},
+    { stat: '3,450+', emphasis: 'Collapsed buildings', rest: 'on people trapped in the ruble.' , source: 'https://www.nytimes.com/2023/02/05/world/europe/turkey-earthquake-rescue.html'},
+    { stat: '23 million', emphasis: 'people affected', rest: 'from the earthquakes.' , source: 'https://abcnews.go.com/International/live-updates/turkey-earthquake/?id=96913081#96947062'},
   ]
   
+  // All the cryptocurrency networks must be listed here
+  const networks = [
+    'Bitcoin',
+    'Ethereum',
+  ];
+
+  // All of the icons should be SVGs
+  // Click "raw" and copy the url from the list at https://github.com/spothq/cryptocurrency-icons/tree/master/svg/color
+  // All option types except "cryptocurrency" and "nft" must have an icon in this map.
+  // All "cryptocurrency" and "nft" types must have exact names in their respective maps.
+  const icons = {
+    cryptocurrencies: {
+      'Bitcoin': 'https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/svg/color/btc.svg',
+      'Ethereum': 'https://raw.githubusercontent.com/spothq/cryptocurrency-icons/master/svg/color/eth.svg',
+    },
+    nfts: {
+      'Cause #1': './icons/cause.svg',
+    },
+    bank: './icons/bank.svg',
+    btcturk: './icons/btcturk.svg',
+  };
+
+  // All entries must have the following: name, description, websiteUrl, endorsementUrls, options
+  // All options must have the following: type, name
+  // If an option has "cryptocurrency" type, it must include: address, sourceUrls
+  // If an option has another type, it must include: linkName, linkUrl
+  // Options could have the following: info, warning
+  const organizations = [
+    {
+      name: 'AFAD (Republic Of Turkey Ministry Of Interior Disaster And Emergency Management Presidency)',
+      description: 'Disaster and Emergency Management Authority, an institution working to prevent disasters and minimize disaster-related damages, plan and coordinate post-disaster response, and promote cooperation among various government agencies.',
+      websiteUrl: 'https://en.afad.gov.tr/',
+      endorsementUrls: [],
+      options: [
+        {
+          type: 'cryptocurrency',
+          name: 'Bitcoin',
+          address: 'bc1q30uv6s9gwyzq2fd5hhc06v7xge3zzg3qa99vuv0w5dfmxgq8vnws97dpcl',
+          sourceUrls: ['https://donation.btcturk.com/', 'https://twitter.com/btcturkpro/status/1623344419976929282'],
+        },
+        {
+          type: 'cryptocurrency',
+          name: 'Ethereum',
+          address: '0x64A994CC850a56e87331d880A23A69b16dbFC8ea',
+          sourceUrls: ['https://donation.btcturk.com/', 'https://twitter.com/btcturkpro/status/1623344419976929282'],
+          warning: 'Only send ETH or USDT as this is an exchange wallet',
+        },
+        {
+          type: 'bank',
+          name: 'Bank accounts',
+          linkName: 'The list of AFAD bank accounts on their official website',
+          linkUrl: 'https://en.afad.gov.tr/earthquake-donation-accounts',
+        },
+        {
+          type: 'btcturk',
+          name: 'BtcTurk',
+          linkName: 'Instructions for donating Turkish Lira from BtcTurk',
+          linkUrl: 'https://pro.btcturk.com/deprem-bagis',
+          info: 'Matched 1:1 by BtcTurk',
+          warning: 'Only Turkish citizens can register to BtcTurk',
+        },
+      ],
+    },
+    {
+      name: 'Earthquake Türkiye 2023 by Murat Pak',
+      description: 'Distinguished Turkish NFT artist Pak has built an NFT platform for raising funds for the relief effort. All donations are promised to be channelled to Ahbap.',
+      websiteUrl: 'https://cause.quest/',
+      endorsementUrls: ['https://twitter.com/beeple/status/1623107218567581697'],
+      options: [
+        {
+          type: 'nft',
+          name: 'Cause #1',
+          linkName: 'Mint Earthquake Türkiye 2023 NFTs',
+          linkUrl: 'https://cause.quest',
+          warning: 'Non-transferable',
+        },
+      ],
+    },
+  ];
+
   return (
     <>
     {/* Hero */}
@@ -68,7 +148,7 @@ export default function Home() {
           </p>
           <div className="mt-12 grid grid-cols-1 gap-y-12 gap-x-6 sm:grid-cols-2">
             {metrics.map((item) => (
-              <p key={item.id}>
+              <p key={item.emphasis}>
                 <span className="block text-2xl font-bold text-white">{item.stat}</span>
                 <span className="mt-1 block text-base text-gray-300">
                   <span className="font-medium text-white">{item.emphasis}</span> {item.rest}
