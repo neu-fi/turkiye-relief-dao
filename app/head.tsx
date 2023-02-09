f9e48c9bd888798c40771eed7ce9d3b8601c7434
Han Tuzun <hantuzun@users.noreply.github.com>
2023-02-09 16:41:09 +0300

    Add programatic og image

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,7 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Help the relief efforts in response to the earthquake." />
       <link rel="icon" href="/favicon.ico" />
+      <meta property="og:image" content="https://turkey-relief-dao.vercel.app/api/og" />
     </>
   )
 }
