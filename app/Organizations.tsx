9eecf0dc89b973a2987522311cb4d08dcf4fbfd2
Han Tuzun <hantuzun@users.noreply.github.com>
2023-02-09 14:37:11 +0300

    Fix address cell alignments

--- app/Organizations.tsx
+++ app/Organizations.tsx
@@ -759,7 +759,7 @@ export default function Organizations() {
                                           : <p className="truncate">{option.linkName}</p>
                                         }
                                       </td>
-                                      <td className="px-3 py-3 text-sm text-gray-500 grow space-x-1 inline-flex">
+                                      <td className="px-3 py-3 text-sm text-gray-500 grow space-x-2 inline-flex justify-end">
                                         {option.info && <div className='flex items-center'>
                                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 align-middle">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
