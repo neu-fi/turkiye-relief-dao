f67633d842cb6c7c141171db1c1943572cbff329
Han Tuzun <hantuzun@users.noreply.github.com>
2023-02-09 11:18:53 +0300

    Move donation stats to right on xl

--- app/DonationStats.tsx
+++ app/DonationStats.tsx
@@ -11,7 +11,7 @@ export default function DonationStats() {
     <div className="relative bg-red-900">
       <div className="absolute bottom-0 h-80 w-full xl:inset-0 xl:h-full">
         <div className="h-full w-full xl:grid xl:grid-cols-2">
-          <div className="h-full xl:relative xl:col-start-2">
+          <div className="h-full xl:relative xl:col-start-1">
             <img
               className="h-full w-full object-cover opacity-25 xl:absolute xl:inset-0"
               src="./help.jpg"
@@ -19,13 +19,13 @@ export default function DonationStats() {
             />
             <div
               aria-hidden="true"
-              className="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-red-900 xl:inset-y-0 xl:left-0 xl:h-full xl:w-32 xl:bg-gradient-to-r"
+              className="absolute top-0 h-32 bg-gradient-to-b from-red-900 xl:inset-y-0 xl:right-0 xl:h-full xl:w-32 xl:bg-gradient-to-l"
             />
           </div>
         </div>
       </div>
       <div className="mx-auto max-w-4xl px-6 lg:max-w-7xl lg:px-8 xl:grid xl:grid-flow-col-dense xl:grid-cols-2 xl:gap-x-8">
-        <div className="relative pt-12 pb-64 sm:pt-24 sm:pb-64 xl:col-start-1 xl:pb-24">
+        <div className="relative pt-12 pb-64 sm:pt-24 sm:pb-64 xl:col-start-2 xl:pb-24">
           <h2 className="text-base font-semibold text-white">Asking for Help</h2>
           <p className="mt-3 text-3xl font-bold tracking-tight text-rose-200">
             The International Response
