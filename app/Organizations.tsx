91cd02644a9fd1daa29c6565bcaa0f76fe2c5bc0
Han Tuzun <hantuzun@users.noreply.github.com>
2023-02-09 15:13:19 +0300

    Add tooltips

--- app/Organizations.tsx
+++ app/Organizations.tsx
@@ -9,7 +9,7 @@ import { LinkIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { Tooltip } from 'flowbite-react'
 
 // All the cryptocurrency networks must be listed here
 const NETWORKS = [
@@ -760,16 +760,26 @@ export default function Organizations() {
                                         }
                                       </td>
                                       <td className="px-3 py-3 text-sm text-gray-500 grow space-x-2 inline-flex justify-end">
-                                        {option.info && <div className='flex items-center'>
-                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 align-middle">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                                          </svg>
-                                        </div>}
-                                        {option.warning && <div className='flex items-center'>
-                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                          <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
-                                        </svg>
-                                        </div>}
+                                        {
+                                          option.info &&
+                                            <Tooltip content={option.info} className="align-center" placement="right">
+                                              <div className='flex items-center'>
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 align-middle">
+                                                  <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                                                </svg>
+                                              </div>
+                                            </Tooltip>
+                                        }
+                                        {
+                                          option.warning &&
+                                            <Tooltip content={option.warning} className="align-center" placement="right">
+                                              <div className='flex items-center'>
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                                                </svg>
+                                              </div>
+                                            </Tooltip>
+                                        }
                                         <button
                                           type="button"
                                           className={
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,14 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js"
+
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('flowbite/plugin'),
   ],
 }
