bdb75839e0575634e1df4091ce20a4c5d7338083
Teena <teenabatra36@gmail.com>
2023-02-12 20:53:13 +0530

    Optimise favicon for different devices

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,10 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Help the relief efforts in response to the earthquake." />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </>
   )
 }
