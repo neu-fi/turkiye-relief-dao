6e459dfec9695832650f0e228a395f73bcdbdbe6
Han Tuzun <hantuzun@users.noreply.github.com>
2023-03-10 05:36:29 -0500

    Update donationStats.ts

--- config/donationStats.ts
+++ config/donationStats.ts
@@ -5,8 +5,8 @@ export default {
   accept cryptocurrencies, and appeal to the global crypto community for support. Our efforts has started to
   make impact. We're grateful for the primarily international donations channelled to the relief efforts.`,
   statistics: [
-    { name: "Unique Addresses", stat: "3,679" },
-    { name: "Contributions", stat: "11,281" },
-    { name: "Total Donations", stat: "$5,517,626" },
+    { name: "Unique Addresses", stat: "3,989" },
+    { name: "Contributions", stat: "11,828" },
+    { name: "Total Donations", stat: "$6,027,978" },
   ],
 };
